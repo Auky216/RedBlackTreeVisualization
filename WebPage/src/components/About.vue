43fe291cbfe0ab8a478d45070b643c48a02742cf
Adrian Antonio Auqui Perez <adrian.auqui@utec.edu.pe>
2023-11-24 21:49:56 -0500

    web final

--- WebPage/src/components/About.vue
+++ WebPage/src/components/About.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="about">
-    <h1>About Us</h1>
+    <h1>Integrantes</h1>
     <div>
       <p>
-        Este proyecto de Algoritmo y Estructuras de Datos del ciclo 2023-2, fue desarrollado por: 
+        Este proyecto de Algoritmo y Estructuras de Datos del ciclo 2023-2, fue desarrollado por alumnos de la Universidad de Ingeniería y Tecnología : 
       </p>
       
       <div class="team">
@@ -33,6 +33,58 @@
         
       </div>
     </div>
+
+    <h1>Tecnologias Usadas</h1>
+
+    <div class="team">
+      <div class="person">
+        <img class="img-profile" src="https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png">
+        <p>Adrian Antonio Auqui Perez</p>
+        <p>19 años</p>
+        <p>Team Python</p>
+        <p>Caminar y hablar</p>
+    </div>
+
+      <div class="person">
+          <img class="img-profile" src="https://upload.wikimedia.org/wikipedia/commons/f/f4/Raylib_logo.png">
+          <p>Adrian Antonio Auqui Perez</p>
+          <p>19 años</p>
+          <p>Team Python</p>
+          <p>Caminar y hablar</p>
+      </div>
+
+      <div class="person">
+        <img class="img-profile" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/WebAssembly_Logo.svg/1200px-WebAssembly_Logo.svg.png">
+        <p>Irán María Alvarez Flores  </p>
+        <p>19 años</p>
+        <p>Team Assembler </p>
+        <p>Ver peliculas y programar</p>
+    </div>
+
+      <div class="person">
+          <img class="img-profile" src="https://avatars.githubusercontent.com/u/6128107?s=280&v=4">
+          <p>Brigitte Dayane Perez</p>
+          <p>19 años</p>
+          <p>Team Python</p>
+          <p>Programar</p>
+      </div>
+
+      <div class="person">
+        <img class="img-profile" src="https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png">
+        <p>Brigitte Dayane Perez</p>
+        <p>19 años</p>
+        <p>Team Python</p>
+        <p>Programar</p>
+    </div>
+      
+    </div>
+
+
+    <h1>Nuestro Paper</h1>
+
+    
+
+    <a href="/src/INFORME FINAL.pdf" download="Red-Black-Tree-Informe-2023-2.pdf">Descargar Nuestro Informe</a>
 
   </div>
 </template>
--- WebPage/src/components/Menu.vue
+++ WebPage/src/components/Menu.vue
@@ -1,38 +1,167 @@
 <template>
   <div class="home">
     <h1>Definicion</h1>
-  <p>Los árboles Red-Black, una forma fascinante de estructurar datos, son conocidos por su equilibrio y capacidad para mantener un rendimiento óptimo en diversas operaciones. En esta sección, exploraremos en profundidad los aspectos esenciales de estos árboles autoequilibrados y cómo impactan en la eficiencia de algoritmos y estructuras de datos.</p>
+    <p>
+      Los árboles Red-Black, una forma fascinante de estructurar datos, son
+      conocidos por su equilibrio y capacidad para mantener un rendimiento
+      óptimo en diversas operaciones. En esta sección, exploraremos en
+      profundidad los aspectos esenciales de estos árboles autoequilibrados y
+      cómo impactan en la eficiencia de algoritmos y estructuras de datos.
+    </p>
 
     <h1>¿Qué son los Red-Black Trees?</h1>
-  <p>Los árboles Red-Black son una forma especial de árboles binarios de búsqueda que poseen propiedades únicas para mantener un equilibrio entre la profundidad del árbol y la complejidad de las operaciones. Están diseñados para optimizar la búsqueda, inserción y eliminación de elementos, ofreciendo un rendimiento predecible y eficiente en una amplia gama de aplicaciones.</p>
-      <center><img src="./icons/homeIcons/1.PNG"></center>
-      <h1>Características Clave      </h1>
-      <p>Estos árboles poseen características particulares que los distinguen de otras estructuras de datos. Sus propiedades fundamentales, como la coloración de nodos y reglas de balanceo, garantizan un comportamiento balanceado y un tiempo de ejecución predecible en operaciones críticas.</p>
-      <h1>Aplicaciones y Uso en la Práctica</h1>
-      <p>Desde algoritmos de búsqueda hasta la implementación de bases de datos y sistemas de archivos, los Red-Black Trees encuentran aplicaciones en diversas áreas de la informática. Su capacidad para mantener un rendimiento estable los convierte en una elección preferida en escenarios donde se requiere un equilibrio entre la velocidad y la complejidad.</p>
-  
+    <p>
+      Los árboles Red-Black son una forma especial de árboles binarios de
+      búsqueda que poseen propiedades únicas para mantener un equilibrio entre
+      la profundidad del árbol y la complejidad de las operaciones. Están
+      diseñados para optimizar la búsqueda, inserción y eliminación de
+      elementos, ofreciendo un rendimiento predecible y eficiente en una amplia
+      gama de aplicaciones.
+    </p>
+    <center><img src="./icons/homeIcons/0.PNG" /></center>
+
+    <h1>Propiedades</h1>
+    
+
+    <div class="div-information">
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/1.PNG" />
+        <p>Cada nodo es rojo o negro.</p>
+     
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/2.PNG" />
+        <p>La raíz siempre es negra.</p>
+ 
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/3.PNG" />
+        <p>Todas las hojas nulas (externas) son negras</p>
+      
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/4.PNG" />
+        <p>Un nodo rojo nunca tiene hijos rojos directos.</p>
+      
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/5.PNG" />
+        <p>Cada camino desde un nodo hacia sus hojas vacías debe tener la misma cantidad de nodos negros.</p>
+      
+      </div>
+    </div>
+
+    
+    <h1>Características Clave</h1>
+    <p>
+      Estos árboles poseen características particulares que los distinguen de
+      otras estructuras de datos. Sus propiedades fundamentales, como la
+      coloración de nodos y reglas de balanceo, garantizan un comportamiento
+      balanceado y un tiempo de ejecución predecible en operaciones críticas.
+    </p>
+
+    <div class="div-information">
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/6.PNG" />
+        <p>Arbol sin balanceo</p>
+     
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/7.PNG" />
+        <p>Balanceando</p>
+        
+        
+      </div>
+
+      <div class="block">
+        <img class="img-profile" src="./icons/homeIcons/8.PNG" />
+        <p>Arbol balanceado</p>
+        
+        
+      </div>
+      
+
       
     </div>
-  
-  
+    <h1>Aplicaciones y Uso en la Práctica</h1>
+    <p>
+      Desde algoritmos de búsqueda hasta la implementación de bases de datos y
+      sistemas de archivos, los Red-Black Trees encuentran aplicaciones en
+      diversas áreas de la informática. Su capacidad para mantener un
+      rendimiento estable los convierte en una elección preferida en escenarios
+      donde se requiere un equilibrio entre la velocidad y la complejidad.
+    </p>
+
+    <center><img src="./icons/homeIcons/10.PNG" /></center>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-embed{
+embed {
   margin: 0 auto;
   display: block;
   width: 100%;
   height: 100vh;
 }
 
-.home{
+.home {
   margin: 10px;
 }
 
+.div-information {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+}
+
+.about {
+  margin: 10px;
+}
+
+.img-profile {
+  width: 200px;
+  height: 200px;
+  border-radius: 20%;
+  margin: 20px;
+}
+.block {
+  /* Restringe el tamaño máximo del cuadro */
+  max-width: 500px;
+  height: 300px;
+  border: 5px solid #000;
+  margin: 10px;
+  transition: all 0.3s ease;
+  position: relative;
+  text-align: center;
+  overflow: hidden; /* Evita desbordamiento horizontal */
+}
+
+.block p {
+  /* Asegura que el texto no haga crecer el cuadro más allá del límite */
+  max-width: 180px; /* Deja algo de espacio para el borde */
+  margin: 0 auto;
+  
+  word-wrap: break-word; /* Permite ajuste de palabras */
+  overflow-y: auto; /* Agrega desplazamiento vertical */
+  max-height: 5em; /* Limita la altura para mostrar el scroll si es necesario */
+}
+
+.block:hover {
+  border-color: #ee3939; /* Cambio del color del borde a rojo */
+  transform: scale(1.1); /* Aumentar tamaño en el hover */
+  z-index: 1; /* Para superposición */
+  box-shadow: 0 0 10px #ee3939; /* Sombra al hacer hover */
+  text-align: center;
+}
 </style>
